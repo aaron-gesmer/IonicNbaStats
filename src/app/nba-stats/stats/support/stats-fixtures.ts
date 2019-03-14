5b6582f5170b86801877ebf0cfd3ba5457750b16
aaron-gesmer <agesmer@cleo.com>
2019-03-14 00:02:24 -0500

    league-stats working

--- src/app/nba-stats/stats/support/stats-fixtures.ts
+++ src/app/nba-stats/stats/support/stats-fixtures.ts
@@ -57,14 +57,10 @@ export const gameScope = [
 export const playerPosition = [
   'F',
   'C',
-  'G',
-  'C-F',
-  'F-C',
-  'F-G',
-  'G-F'
+  'G'
 ];
 
-export const pointDiff = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+export const pointDiff = [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ];
 
 export const ptMeasureType = [
   'SpeedDistance',
--- src/pages/nba-stats-pages/league-stats/league-stats.ts
+++ src/pages/nba-stats-pages/league-stats/league-stats.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the LeaguePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { NbaStatsService } from '../../../app/nba-stats/nba-stats.service';
+import * as statsFixtures from '../../../app/nba-stats/stats/support/stats-fixtures';
 
 @IonicPage()
 @Component({
@@ -14,13 +10,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'league-stats.html',
 })
 export class LeagueStatsPage {
-  
+  result = '';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  // Set up params:
+  gameScopeOptions = statsFixtures.gameScope;
+  playerTeamOptions = ['Player', 'Team'];
+  playerScopeOptions = ['All Players', 'Rookies'];
+  statTypeOptions = statsFixtures.statType;
+  ptMeasureTypeOptions = statsFixtures.ptMeasureType;
+  playerPositionOptions = statsFixtures.playerPosition;
+  
+  // Default values:
+  gameScope = this.gameScopeOptions[0];
+  playerOrTeam = this.playerTeamOptions[0];
+  playerScope = this.playerScopeOptions[0];
+  statType = this.statTypeOptions[0];
+  ptMeasureType = this.ptMeasureTypeOptions[0];
+  playerPosition = '';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private nbaStatsService: NbaStatsService) { }
+
+  async getHomepage() {
+    const homepage = await this.nbaStatsService.getHomepage(this.gameScope, this.playerOrTeam, this.playerScope, this.statType);
+    this.print(homepage);
+  }
+
+  async getPlayerTracking() {
+    const playerTracking = await this.nbaStatsService.getPlayerTracking(this.ptMeasureType, this.playerPosition);
+    this.print(playerTracking);
+  }
+
+  private print(object: any) {
+    this.result = JSON.stringify(object, null, 2);
+    console.log(this.result);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LeaguePage');
   }
-
 }
